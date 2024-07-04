047481337cab9869d5555a66f4448e857ec780c2
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2024-07-05 00:27:28 +0300

    migrate to another discord rpc library

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,60 @@
-use gsmtc::{ManagerEvent::*, SessionUpdateEvent::*};
 
+use std::sync::Arc;
+
+use gsmtc::{
+    ManagerEvent::*,
+    SessionModel,
+    SessionUpdateEvent::{self, *},
+};
+use rpc::YandexMusicStateBuilder;
+
+mod rpc;
 mod tray;
+
+//TODO: Old client
+const TARGET_SOURCES: &[&str] = &["ru.yandex.desktop.music"];
 
 #[tokio::main]
 async fn main() {
+    let state_sender = Arc::new(rpc::init().await);
+
     let task = tray::start();
 
     let mut rx = gsmtc::SessionManager::create().await.unwrap();
 
     while let Some(evt) = rx.recv().await {
-        if let SessionCreated { session_id, mut rx, source } = evt {
+        if let SessionCreated {
+            session_id,
+            mut rx,
+            source,
+        } = evt
+        {
             println!("Created session: {{id={session_id}, source={source}}}");
+            let state_sender = state_sender.clone();
             tokio::spawn(async move {
                 while let Some(evt) = rx.recv().await {
                     if let Media(model, image) = evt {
                         println!("[{session_id}/{source}] Media updated: {model:#?} - {image:?}");
+
+                        let SessionModel {
+                            playback,
+                            timeline: _,
+                            media,
+                            source,
+                        } = model;
+
+                        if TARGET_SOURCES.contains(&source.as_str()) {
+                            let media = media.unwrap();
+
+                            state_sender.send(
+                                YandexMusicStateBuilder::new()
+                                    .state(playback.unwrap().status.into())
+                                    .album(media.album.map(|x| x.title).unwrap_or("".to_string()))
+                                    .artist(media.artist)
+                                    .track(media.title)
+                                    .build()
+                            ).await.unwrap();
+                        }
                     }
                 }
             });
---
+++ src/rpc.rs
@@ -0,0 +1,127 @@
+use std::sync::{mpsc::Receiver, LazyLock, Mutex};
+
+use discord_rich_presence::{activity::{Activity, Assets}, DiscordIpc, DiscordIpcClient};
+
+use dotenv_codegen::dotenv;
+use gsmtc::PlaybackStatus;
+
+#[derive(Clone, Copy)]
+pub enum State {
+    Playing,
+    Paused,
+}
+
+impl Into<State> for PlaybackStatus {
+    fn into(self) -> State {
+        match self {
+            PlaybackStatus::Playing => State::Playing,
+            _ => State::Paused,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct YandexMusicState {
+    pub track: String,
+    pub artist: String,
+    pub album: String,
+    pub state: State,
+}
+
+pub struct YandexMusicStateBuilder {
+    state: YandexMusicState,
+}
+
+impl YandexMusicStateBuilder {
+    pub fn new() -> Self {
+        Self {
+            state: YandexMusicState {
+                track: String::new(),
+                artist: String::new(),
+                album: String::new(),
+                state: State::Paused,
+            },
+        }
+    }
+
+    pub fn track(mut self, track: String) -> Self {
+        self.state.track = track;
+        self
+    }
+
+    pub fn artist(mut self, artist: String) -> Self {
+        self.state.artist = artist;
+        self
+    }
+
+    pub fn album(mut self, album: String) -> Self {
+        self.state.album = album;
+        self
+    }
+
+    pub fn state(mut self, state: State) -> Self {
+        self.state.state = state;
+        self
+    }
+
+    pub fn build(self) -> YandexMusicState {
+        self.state
+    }
+}
+
+pub struct RPC {
+    client: DiscordIpcClient,
+}
+
+impl RPC {
+    pub fn set_state(&mut self, state: YandexMusicState) {
+        let YandexMusicState {
+            track,
+            artist,
+            album,
+            state,
+        } = state;
+
+        self.client
+            .set_activity( Activity::new()
+                .details(&format!("🎵 {track}"))
+                    .state(&format!("👤 {artist}"))
+                    .assets(
+                        Assets::new()
+                            .large_image("logo")
+                            .large_text(&format!("💿 {album}"))
+                            .small_image(match state {
+                                State::Playing => "playing",
+                                State::Paused => "paused",
+                            })
+                            .small_text(match state {
+                                State::Playing => "▶️ Playing",
+                                State::Paused => "⏸️ Paused",
+                            })
+                    )
+                )
+            .unwrap();
+    }
+}
+
+pub async fn init() -> tokio::sync::mpsc::Sender<YandexMusicState> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<YandexMusicState>(10);
+
+    tokio::spawn(async move {
+        let mut rpc = RPC {
+            client: DiscordIpcClient::new(dotenv!("CLIENT_ID")).unwrap(),
+        };
+
+        println!("Client ID: {}", dotenv!("CLIENT_ID"));
+
+        rpc.client.connect().unwrap();
+
+        println!("Connected");
+
+        while let Some(evt) = rx.recv().await {
+            rpc.set_state(evt);
+        }
+    });
+
+    tx
+}
